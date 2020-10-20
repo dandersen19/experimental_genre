bc818dd85aeed90690f7a0371b790178df37c738
Drake Andersen <53623035+dandersen19@users.noreply.github.com>
2020-10-20 12:41:21 -0400

    Create substring_function.py

---
+++ code/substring_function.py
@@ -0,0 +1,15 @@
+import pandas as pd
+data = pd.read_csv("/Users/drakeandersen/data science/list.csv")
+
+# function
+def find_genre(genre):
+	total = 0
+	for i in data.index:
+		if genre.lower() in data['Genre'][i].lower():
+			total = data['Count'][i] + total
+	print(total)
+
+# then try various substrings
+find_genre("rock")
+find_genre("classical")
+find_genre("hip hop")
